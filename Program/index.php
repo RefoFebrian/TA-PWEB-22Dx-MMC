0d9dd38ff498ccb2b213e8b2a97eef44e255541d
heruarbianto <heru_arbianto@teknokrat.ac.id>
2024-04-25 09:47:10 +0700

    HERU: membuat form login

--- Program/index.php
+++ Program/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Klinik</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
@@ -14,10 +14,10 @@
         <div class="logo">
             <img src="logo.png" alt="Logo">
         </div>
-        <div class="name">Klinik Pratama Sumbersion</div>
-        <form action="cek-login.php" method="post">
+        <div class=" text-center mt-4 name">Klinik Pratama Sumbersion</div>
+        <form class="p-3 mt-4" action="cek-login.php" method="post">
             <div class="form-field d-flex align-items-center">
-                <span class="far fa-user"></span>
+                <span class="fas fa-user"></span>
                 <input type="text" name="username" id="username" placeholder="Username">
             </div>
             <div class="form-field d-flex align-items-center">
