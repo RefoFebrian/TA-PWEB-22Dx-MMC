46879f59b063c36ff52f3b3eb3da2f12916fa3a6
rakaagisaputra <raka_agi_saputra@teknorat.ac.id>
2024-04-26 22:11:13 +0700

    raka: menambahkan fungsi crud_create() untuk menampilkan formulir penambahan data pengguna

--- Program/pengguna.php
+++ Program/pengguna.php
@@ -1,93 +1,130 @@
 <html>
-    <?php
-    // Mengambil nilai 'a' dari query string jika tidak kosong, jika kosong maka diatur sebagai "reset"
-    $a = !empty($_GET['a']) ? $_GET['a'] : "reset";
+<?php
+// Mengambil nilai 'a' dari query string jika tidak kosong, jika kosong maka diatur sebagai "reset"
+$a = !empty($_GET['a']) ? $_GET['a'] : "reset";
 
-    // Mengambil nilai 'id' dari query string jika tidak kosong, jika kosong maka diatur sebagai string kosong
-    $id_user = !empty($_GET['id']) ? $_GET['id'] : " ";
+// Mengambil nilai 'id' dari query string jika tidak kosong, jika kosong maka diatur sebagai string kosong
+$id_user = !empty($_GET['id']) ? $_GET['id'] : " ";
 
-    // Menggunakan fungsi koneksidatabase() untuk mendapatkan koneksi ke database
-    $kdb = koneksidatabase();
+// Menggunakan fungsi koneksidatabase() untuk mendapatkan koneksi ke database
+$kdb = koneksidatabase();
 
-    // Mengambil nilai 'a' dari query string tanpa menangani error
-    $a = @$_GET["a"];
+// Mengambil nilai 'a' dari query string tanpa menangani error
+$a = @$_GET["a"];
 
-    // Mengambil nilai 'sql' dari data yang diupload tanpa menangani error
-    $sql = @$_POST["sql"];
+// Mengambil nilai 'sql' dari data yang diupload tanpa menangani error
+$sql = @$_POST["sql"];
 
-    // Menggunakan switch case untuk memeriksa nilai dari $sql lalu menjalankan fungsi yang dipanggil
-    switch ($sql) {
-        case "insert":
-            sql_insert();
-            break;
-        case "update":
-            sql_update();
-            break;
-        case "delete":
-            sql_delete();
-            break;
-    }
+// Menggunakan switch case untuk memeriksa nilai dari $sql lalu menjalankan fungsi yang dipanggil
+switch ($sql) {
+    case "insert":
+        sql_insert();
+        break;
+    case "update":
+        sql_update();
+        break;
+    case "delete":
+        sql_delete();
+        break;
+}
 
-    // Menggunakan switch case untuk memeriksa nilai dari $a dan mengeksekusi fungsi yang sesuai
-    switch ($a) {
-        case "reset":
-            curd_read();
-            break;
-        case "tambah":
-            curd_create();
-            break;
-        case "edit":
-            // Memanggil fungsi curd_update() dengan $id_user sebagai parameter
-            curd_update($id_user);
-            break;
-        case "hapus":
-            // Memanggil fungsi curd_delete() dengan $id_user sebagai parameter
-            curd_delete($id_user);
-            break;
-        default:
-            curd_read();
-            break;
-    }
+// Menggunakan switch case untuk memeriksa nilai dari $a dan mengeksekusi fungsi yang sesuai
+switch ($a) {
+    case "reset":
+        curd_read();
+        break;
+    case "tambah":
+        curd_create();
+        break;
+    case "edit":
+        // Memanggil fungsi curd_update() dengan $id_user sebagai parameter
+        curd_update($id_user);
+        break;
+    case "hapus":
+        // Memanggil fungsi curd_delete() dengan $id_user sebagai parameter
+        curd_delete($id_user);
+        break;
+    default:
+        curd_read();
+        break;
+}
+mysqli_close($kdb);
+// Lanjutkan pembuatan fungsi CRUD
 
-    // Lanjutkan pembuatan fungsi CRUD
 
-    
-    // Fungsi Koneksi Ke database
-    function koneksidatabase()
-    {
-        // Mengimpor file koneksi.php untuk mendapatkan koneksi ke database
-        include('koneksi.php');
+// Fungsi Koneksi Ke database
+function koneksidatabase()
+{
+    // Mengimpor file koneksi.php untuk mendapatkan koneksi ke database
+    include('koneksi.php');
 
-        // Mengembalikan koneksi database
-        return $kdb;
-    }
-    ?>
+    // Mengembalikan koneksi database
+    return $kdb;
+}
+?>
 
-    <!-- Fungsi Formeditor -->
-    <?php
+<!-- Fungsi Formeditor -->
+<?php
 function formeditor($row)
-    {
-    ?>
-        <center>
-            <table>
-                <tr>
-                    <td width="200px">Nama user</td>
-                    <td><input type="text" name="username" id="username" maxlength="25" size="25" value="<?php echo trim($row["username"]) ?>"></td>
-                </tr>
-                <tr>
-                    <td width="200px">password</td>
-                    <td><input type="text" name="password" id="password" maxlength="25" size="25" value="<?php echo trim($row["password"]) ?>"></td>
-                </tr>
-                <tr>
-                    <td width="200px">hak</td>
-                    <td><select name="hak">
+{
+?>
+    <center>
+        <table>
+            <tr>
+                <td width="200px">Nama user</td>
+                <td><input type="text" name="username" id="username" maxlength="25" size="25" value="<?php echo trim($row["username"]) ?>"></td>
+            </tr>
+            <tr>
+                <td width="200px">password</td>
+                <td><input type="text" name="password" id="password" maxlength="25" size="25" value="<?php echo trim($row["password"]) ?>"></td>
+            </tr>
+            <tr>
+                <td width="200px">hak</td>
+                <td><select name="hak">
                         <option value="admin">Admin</option>
                         <option value="pegawai">Pegawai</option>
                         <option value="apotek">Apotek</option>
                         <option value="kasir">Kasir</option>
                     </select>
-                    </td>
-                </tr>
-            </table>
-        </center>
-    <?php  } ?>
+                </td>
+            </tr>
+        </table>
+    </center>
+<?php  } ?>
+
+
+<!-- fungsi crud create -->
+<?php
+// Fungsi untuk membuat tampilan form penambahan data pengguna
+function curd_create()
+{
+?><center>
+        <!-- Judul form -->
+        <h3>Penambahan Data user</h3><br>
+
+        <!-- Tombol untuk membatalkan penambahan data -->
+        <a href="pengguna.php?a=reset" class="btn btn-danger">Batal</a>
+        <br>
+
+        <!-- Form untuk mengirimkan data penambahan pengguna -->
+        <form action="pengguna.php?a=reset" method="post">
+            <!-- Input tersembunyi untuk menandai tipe operasi SQL -->
+            <input type="hidden" name="sql" value="insert">
+
+            <?php
+            // Data pengguna awal dengan nilai default
+            $row = array(
+                "username" => "",
+                "password" => "",
+                "hak" => ""
+            );
+
+            // Memanggil fungsi untuk menampilkan form pengisian data pengguna
+            formeditor($row)
+            ?>
+
+            <!-- Tombol untuk menyimpan data pengguna yang ditambahkan -->
+            <p><input type="submit" name="action" value="Simpan" class="btn btn-success "></p>
+        </form>
+    </center>
+<?php } ?>
